c0137c7e89e9969b4089f72b5e9d6bfe5fc34716
salini <Salini.Kannan@trumpf.com>
2026-07-23 15:38:27 +0530

    A02 Assignment

---
+++ A02/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace A02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      int n = new Random ().Next (1, 101);
+      for(; ; ) {
+         Console.WriteLine ("Enter the number you have gussed:");
+         if(!int.TryParse(Console.ReadLine(),out int r)){
+            Console.WriteLine ("Please enter a valid number");
+            continue;
+         }
+         if (r == n) {
+            Console.WriteLine ("You have guessed the number correctly!!");
+            break;
+         }
+         else if (r < n) {
+            Console.WriteLine ("your guess is too low");
+         } else {
+            Console.WriteLine ("your guess is too high");
+         }
+      }
+    }
+}
