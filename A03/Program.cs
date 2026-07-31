547ee6dd6d2db74a142c6d111246081a7956d334
salini <Salini.Kannan@trumpf.com>
2026-07-31 12:55:26 +0530

    A03: spelling bee

--- A03/Program.cs
+++ A03/Program.cs
@@ -1,6 +1,7 @@
-﻿class Program {
+﻿using static System.Console;
+class Program {
    static void Main () {
-      char[] letters = { 'U', 'X', 'A', 'L', 'T', 'N', 'E' };
+      char[] letters = {'U', 'X', 'A', 'L', 'T', 'N', 'E'};
       char requiredLetter = letters[0];
       List<(string Word, int Score, bool Pangram)> results = new ();
       foreach (string line in File.ReadLines ("C:\\Users\\kannansa\\Downloads\\words 1.txt")) {
@@ -11,7 +12,7 @@
             if (word.Length == 4) score = 1;
             else score = word.Length;
             if (pangram) score += 7;
-            results.Add ((word.ToUpper (), score, pangram));
+            results.Add ((word, score, pangram));
          }
       }
       results = results
@@ -19,29 +20,26 @@
           .ThenBy (x => x.Word)
           .ToList ();
       int total = 0;
-
       foreach (var item in results) {
          total += item.Score;
-
-         if (item.Pangram) Console.ForegroundColor = ConsoleColor.Green;
-         else Console.ResetColor ();
-         Console.WriteLine ($"{item.Score,2}. {item.Word}");
+         if (item.Pangram) ForegroundColor = ConsoleColor.Green;
+         else ResetColor ();
+         WriteLine ($"{item.Score,2}. {item.Word}");
       }
-      Console.ResetColor ();
-      Console.WriteLine ("-----");
-      Console.WriteLine ($"{total} total");
+      ResetColor ();
+      WriteLine ($"-----\n{total} total");
    }
+
    static bool IsValidWord (string word, char required, char[] allowed) {
       if (word.Length < 4) return false;
-
       if (!word.Contains (required)) return false;
-
       foreach (char c in word) {
          if (!allowed.Contains (c))
             return false;
       }
       return true;
    }
+
    static bool IsPangram (string word, char[] allowed) {
       foreach (char c in allowed) {
          if (!word.Contains (c))
