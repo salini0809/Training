cd3f44d76d9969568d9bf3ac6a1fcd414e6c2d1d
salini <Salini.Kannan@trumpf.com>
2026-08-04 14:33:47 +0530

    modified loop and valid input condition

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,25 +1,33 @@
-﻿namespace A02;
-
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2026.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------------------------------------
+// Program.cs
+// Program to generate a random number between 1 and 100 and allow the user to guess it.
+// ------------------------------------------------------------------------------------------------
+using static System.Console;
 class Program
 {
     static void Main(string[] args)
     {
       int n = new Random ().Next (1, 101);
-      for(; ; ) {
-         Console.WriteLine ("Enter the number you have gussed:");
-         if(!int.TryParse(Console.ReadLine(),out int r)){
-            Console.WriteLine ("Please enter a valid number");
+      int attempts = 1;
+      while (true){
+         WriteLine ("Enter the number you have guessed between 1 and 100 :");
+         if(!int.TryParse (ReadLine (), out int r) || r < 1 || r > 100){
+            WriteLine ("Please enter a valid number");
             continue;
          }
          if (r == n) {
-            Console.WriteLine ("You have guessed the number correctly!!");
+            WriteLine ($"You have guessed the number correctly in {attempts} attempts!!");
             break;
          }
          else if (r < n) {
-            Console.WriteLine ("your guess is too low");
+            WriteLine ("your guess is too low");
          } else {
-            Console.WriteLine ("your guess is too high");
+            WriteLine ("your guess is too high");
          }
+         attempts++;
       }
     }
 }
