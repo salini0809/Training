2c4b4bff2a0fd15c7bcd4d550e3ac278908011cf
salini <Salini.Kannan@trumpf.com>
2026-08-10 17:45:47 +0530

    Added functionaity to play multiple times

--- A02/Program.cs
+++ A02/Program.cs
@@ -5,29 +5,35 @@
 // Program.cs
 // Program to generate a random number between 1 and 100 and allow the user to guess it.
 // ------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 using static System.Console;
-class Program
-{
-    static void Main(string[] args)
-    {
-      int n = new Random ().Next (1, 101);
-      int attempts = 1;
-      while (true){
-         WriteLine ("Enter the number you have guessed between 1 and 100 :");
-         if(!int.TryParse (ReadLine (), out int r) || r < 1 || r > 100){
+class Program {
+   static void Main (string[] args) {
+      do {
+         PlayGame ();
+      } while (PlayAgain ());
+      WriteLine ("Thank you for playing!");
+   }
+   static void PlayGame () {
+      int SecretNumber = new Random ().Next (1, 101), attempts = 0, guess;
+      do {
+         Write ("Enter the number you have guessed between 1 and 100 : ");
+         if (!int.TryParse (ReadLine (), out guess) || guess < 1 || guess > 100) {
             WriteLine ("Please enter a valid number");
             continue;
          }
-         if (r == n) {
-            WriteLine ($"You have guessed the number correctly in {attempts} attempts!!");
-            break;
-         }
-         else if (r < n) {
-            WriteLine ("your guess is too low");
-         } else {
-            WriteLine ("your guess is too high");
-         }
          attempts++;
+         WriteLine (guess == SecretNumber ? $"You have guessed the number in {attempts} attempts."
+                   : guess < SecretNumber ? "Your guess is too low." : "Your guess is too high.");
+      } while (guess != SecretNumber);
+   }
+   static bool PlayAgain () {
+      while (true) {
+         Write ("Do you want to play again? (y/n): ");
+         string? input = ReadLine ()?.ToLower ();
+         if (input == "y" || input == "n")
+            return input == "y" ? true : false;
+         WriteLine ("Invalid input. Please enter (Y)es or (N)o.");
       }
-    }
+   }
 }
