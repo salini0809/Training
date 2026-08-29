fbbdefaeb01e029b5f6b4b5dbf34088ef9b23a91
salini <Salini.Kannan@trumpf.com>
2026-08-29 11:49:49 +0530

    Added color codded console messages

--- A02.2/Program.cs
+++ A02.2/Program.cs
@@ -3,9 +3,10 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------------------------------------
 // Program.cs
-// Program to guess a number between 1 and 127 by asking the user questions based on the remainder.
+// Program to guess a number between 0 and 127 by asking the user questions based on the remainder.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
+using static System.ConsoleColor;
 
 class Program {
    static void Main (string[] args) {
@@ -13,25 +14,25 @@ class Program {
       WriteLine ("Please answer my questions with (Y)es or (N)o");
       int number = 0, divisor = 2, remainder = 1;
       for (int i = 0; i < 7; i++) {
-         Write ($"\nWhen divided by {divisor}, is the remainder >= {remainder} ? ");
-         int result = ProcessAnswer (number, remainder);
-         if (result == -1) {
-            i--;
-            continue;
+         Write ($"When divided by {divisor}, is the remainder >= {remainder} ? ");
+         switch (ReadKey (true).Key) {
+            case ConsoleKey.Y:
+               number += remainder;
+               PrintMsg ("Y", Blue); break;
+            case ConsoleKey.N: PrintMsg ("N", Magenta); break;
+            default:
+               WriteLine ("\nInvalid Input", Red);
+               i--; continue;
          }
-         number = result;
-         divisor *= 2;
-         remainder *= 2;
+         divisor *= 2; remainder *= 2;
       }
-      WriteLine ($"\nThe number you thought of is {number}");
+      PrintMsg ($"The number you thought of is {number}", Green);
    }
-   static int ProcessAnswer (int number, int remainder) {
-      switch (ReadKey ().Key) {
-         case ConsoleKey.Y: return number + remainder;
-         case ConsoleKey.N: return number;
-         default:
-            WriteLine ("\nInvalid Input");
-            return -1;
-      }
+
+   //Prints the specified message in the given color.
+   static void PrintMsg (string msg, ConsoleColor color) {
+      ForegroundColor = color;
+      WriteLine (msg);
+      ResetColor ();
    }
 }
