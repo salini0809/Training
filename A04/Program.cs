f8320572f693c7dd1592ad952a88809d8b14f481
salini <Salini.Kannan@trumpf.com>
2026-09-10 14:29:10 +0530

    A04:Spelling Bee -Letter Frequency

---
+++ A04/Program.cs
@@ -0,0 +1,20 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2026.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------------------------------------
+// Program.cs
+// Finds the frequency of all letters in a word list and displays the top 7 most frequent letters.
+// ------------------------------------------------------------------------------------------------
+using static System.Console;
+
+string[] words = File.ReadAllLines ("words.txt");
+Dictionary<char, int> frequency = [];
+foreach (string word in words) {
+   foreach (char ch in word) {
+      if (char.IsLetter (ch))
+         frequency[ch] = frequency.GetValueOrDefault (ch) + 1;
+   }
+}
+WriteLine ("Seven most frequently occuring letters in the word list are:");
+foreach (var item in frequency.OrderByDescending (x => x.Value).Take (7))
+   WriteLine ($"{item.Key} : {item.Value}");
