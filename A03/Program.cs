57e37e59214da24a41513432c72b17855b124530
salini <Salini.Kannan@trumpf.com>
2026-09-22 09:54:46 +0530

    Review Changes Fixed

--- A03/Program.cs
+++ A03/Program.cs
@@ -10,9 +10,9 @@ using static System.Console;
 using static System.ConsoleColor;
 
 class Program {
-   static char[] letters = ['U', 'X', 'A', 'L', 'T', 'N', 'E'];
-   static List<(string Word, int Score, bool Pangram)> results = [];
    static void Main () {
+      char[] letters = ['U', 'X', 'A', 'L', 'T', 'N', 'E'];
+      List<(string Word, int Score, bool Pangram)> results = [];
       string[] words = File.ReadAllLines ("words.txt");
       int total = 0;
       foreach (string word in words.Select (word => word.Trim ()).Where (IsValidWord)) {
@@ -23,14 +23,15 @@ class Program {
       results = [.. results.OrderByDescending (x => x.Score).ThenBy (x => x.Word)];
       foreach (var (Word, Score, Pangram) in results) {
          total += Score;
-         ForegroundColor = Pangram ? Green : White;
+         if (Pangram) ForegroundColor = Green;
+         else ResetColor ();
          WriteLine ($"{Score,2}. {Word}");
       }
       ResetColor ();
       WriteLine ($"----\n{total} total");
-   }
 
-   // Checks if the word is valid based on length, required letter, and allowed letters.
-   static bool IsValidWord (string word)
-      => word.Length >= 4 && word.Contains (letters[0]) && word.All (letters.Contains);
+      // Checks if the word is valid based on length, required letter, and allowed letters.
+      bool IsValidWord (string word)
+         => word.Length >= 4 && word.Contains (letters[0]) && word.All (letters.Contains);
+   }
 }
